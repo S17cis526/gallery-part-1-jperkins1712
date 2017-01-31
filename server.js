766d25326c61e15bdb84b3eadd62a05b351d67d6
jperkins1712 <joey.perkins.school@gmail.com>
2017-01-31 14:25:59 -0600

    January 31st
    In class workd for Jan 31st -- setting up a gallery.

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ var fs = require('fs');
 
 var port = 1712;  // nonstandard port
 
+var stylesheet = fs.readFileSync('gallery.css');
+var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+
 function serveImage(filename, req, res) {
   var body = fs.readFile('images/' + filename, function(err, body) {
     if (err) {
@@ -34,17 +37,41 @@ function serveImage(filename, req, res) {
 // creates server but its not listening yet
 var server = http.createServer(function(req, res) {
   switch (req.url) {
+    case '/gallery':
+      // performs function on all fileNames -- makes array out of them -- joins them with a delimitor
+      var gHtml = imageNames.map(function(fileName) {
+        return ' <img src="' + fileName + '"alt="a fishing ace at work">';
+      }).join('');
+      var html = "<!doctype html>";
+        html += '<head>';
+        html +=   '<title>Gallery</title>';
+        html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">';
+        html += '</head>';
+        html += '<body>';
+        html += ' <h1>Gallery</h1>';
+        html += gHtml;
+        html += '</body>';
+      res.setHeader('Content-Type', 'text/html');
+      res.end(html);
+      break;
     case "/chess":
     case "/chess/":
+    case "/chess.jpg":
       serveImage('chess.jpg', req, res);
       break;
     case "/ace":
     case "/ace/":
+    case "/ace.jpg":
       serveImage('ace.jpg', req, res);
       break;
     case "/fern":
     case "/fern/":
+    case "/fern.jpg":
       serveImage('fern.jpg', req, res);
+      break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
       break;
     default:
       res.statusCode = 404;
