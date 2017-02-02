22bf1be6c76d0e361329166476fcd95200233357
jperkins1712 <joey.perkins.school@gmail.com>
2017-02-02 14:16:19 -0600

    Feb 2nd
    In class work from Feb 2nd -- gallery now handles any number of images
    in the images folder.

--- server.js
+++ server.js
@@ -17,6 +17,22 @@ var port = 1712;  // nonstandard port
 var stylesheet = fs.readFileSync('gallery.css');
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
 
+// returns an array of the image names in /images
+function getImageNames(callback) {
+  fs.readdir('images/', function(err, fileNames) {
+    if (err) callback(err, null);
+    else callback(false, fileNames);
+  });
+}
+
+// takes in a file name and returns the file tag as a string
+function imageNamesToTags(fileNames) {
+  return fileNames.map(function(fileName) {
+    return `<img src="${fileName}" alt="${fileName}">`;
+  });
+}
+
+// reads an image and returns it
 function serveImage(filename, req, res) {
   var body = fs.readFile('images/' + filename, function(err, body) {
     if (err) {
@@ -34,49 +50,49 @@ function serveImage(filename, req, res) {
   });
 }
 
+function buildGallery(imageTags) {
+  var html = "<!doctype html>";
+    html += '<head>';
+    html +=   '<title>Gallery</title>';
+    html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">';
+    html += '</head>';
+    html += '<body>';
+    html += ' <h1>Gallery</h1>';
+    html += imageNamesToTags(imageTags).join('');
+    html += '</body>';
+  return html;
+}
+
+function serveGallery(req, res) {
+  getImageNames(function(err, imageNames) {
+    if (err) {
+      console.err(err);
+      res.statusCode = 500;
+      res.statusMessage = 'Server Error';
+      res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end(buildGallery(imageNames));
+
+  });
+
+}
+
 // creates server but its not listening yet
 var server = http.createServer(function(req, res) {
   switch (req.url) {
+    case '/':
     case '/gallery':
-      // performs function on all fileNames -- makes array out of them -- joins them with a delimitor
-      var gHtml = imageNames.map(function(fileName) {
-        return ' <img src="' + fileName + '"alt="a fishing ace at work">';
-      }).join('');
-      var html = "<!doctype html>";
-        html += '<head>';
-        html +=   '<title>Gallery</title>';
-        html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">';
-        html += '</head>';
-        html += '<body>';
-        html += ' <h1>Gallery</h1>';
-        html += gHtml;
-        html += '</body>';
-      res.setHeader('Content-Type', 'text/html');
-      res.end(html);
-      break;
-    case "/chess":
-    case "/chess/":
-    case "/chess.jpg":
-      serveImage('chess.jpg', req, res);
-      break;
-    case "/ace":
-    case "/ace/":
-    case "/ace.jpg":
-      serveImage('ace.jpg', req, res);
-      break;
-    case "/fern":
-    case "/fern/":
-    case "/fern.jpg":
-      serveImage('fern.jpg', req, res);
+      serveGallery(req, res);
       break;
     case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
       res.end(stylesheet);
       break;
     default:
-      res.statusCode = 404;
-      res.statusMessage = "Not Found";
-      res.end();
+      serveImage(req.url, req, res);
+      
   }
 
 
