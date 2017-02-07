46b63b28fc8aa585a27564d80f983dbd5ccb080b
jperkins1712 <joey.perkins.school@gmail.com>
2017-02-07 14:23:45 -0600

    Feb 7th
    In class work from Feb 7th. Can change title and almost upload images.

--- server.js
+++ server.js
@@ -9,10 +9,13 @@
 //http object using node object
 var http = require('http');
 
+var url = require('url');
+
 // filesystem object using node library
 var fs = require('fs');
 
 var port = 1712;  // nonstandard port
+var config = JSON.parse(fs.readFileSync('config.json') || defaultConfig);
 
 var stylesheet = fs.readFileSync('gallery.css');
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
@@ -53,12 +56,19 @@ function serveImage(filename, req, res) {
 function buildGallery(imageTags) {
   var html = "<!doctype html>";
     html += '<head>';
-    html +=   '<title>Gallery</title>';
+    html +=   '<title>'+ config.title + '</title>';
     html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">';
     html += '</head>';
     html += '<body>';
-    html += ' <h1>Gallery</h1>';
+    html += ' <h1>'+ config.title +'</h1>';
+    html += '  <form>';
+    html += '   <input type="text" name="title">';
+    html += '   <input type="submit" value="Change Gallery Title">';
+    html += ' </form>';
     html += imageNamesToTags(imageTags).join('');
+    html += '<form action="" method="POST" enctype="multipart/form-data">';
+    html += ' <input type="file" name="image">';
+    html += ' <input type="submit" Value="Upload Image">';
     html += '</body>';
   return html;
 }
@@ -79,12 +89,56 @@ function serveGallery(req, res) {
 
 }
 
+function uploadPicture(req, res) {
+  var body='';
+  req.on('error', function() {
+    res.statusCode = 500;
+    res.statusMessage = "Error uploading image.";
+    res.end();
+  });
+  req.on('data', function() {
+    body += data;
+  });
+  req.on('end', function() {
+    fs.writeFile('filename', body, function(err) {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.statusMessage = "Error while uploading."
+        res.end();
+        return;
+      }
+
+      serveGallery(req, res);
+    });
+  });
+
+}
+
 // creates server but its not listening yet
 var server = http.createServer(function(req, res) {
-  switch (req.url) {
+
+  // splits the url into two parts -- a resource and a queryString split by ?
+
+  var urlParts = url.parse(req.url);
+
+  if (urlParts.query) {
+    var matches = /title=(.+)($|&)/.exec(urlParts.query);
+    if (matches && matches[1]) {
+      config.title = decodeURIComponent(matches[1]);
+      fs.writeFile('config.json', JSON.stringify(config));
+    }
+    //looking for any number of chars (.) repeated any number of times (+) ended by end of string (^&)
+  }
+
+  switch (urlParts.pathname) {
     case '/':
     case '/gallery':
-      serveGallery(req, res);
+      if (req.method == 'GET') {
+      serveGallery(req, res); }
+      else if (req.method == 'POST') {
+        uploadPicture(req, res); // consumes POST request, generates response
+      }
       break;
     case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
@@ -92,7 +146,7 @@ var server = http.createServer(function(req, res) {
       break;
     default:
       serveImage(req.url, req, res);
-      
+
   }
 
 
