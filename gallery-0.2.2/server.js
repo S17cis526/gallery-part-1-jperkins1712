d9a2486c2b1ecece0ee820c4c29c57e06bd35568
jperkins1712 <joey.perkins.school@gmail.com>
2017-02-16 14:20:51 -0600

    in class feb 16

--- gallery-0.2.2/server.js
+++ gallery-0.2.2/server.js
@@ -10,11 +10,14 @@ var multipart = require('./multipart');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var port = 3000;
+var port = 1712;
+var template = require('./template');
 
 /* load cached files */
 var config = JSON.parse(fs.readFileSync('config.json'));
 var stylesheet = fs.readFileSync('gallery.css');
+
+template.loadDir('templates');
 
 /** @function getImageNames
  * Retrieves the filenames for all images in the
@@ -50,24 +53,10 @@ function imageNamesToTags(fileNames) {
  * gallery images.
  */
 function buildGallery(imageTags) {
-  var html =  '<!doctype html>';
-      html += '<head>';
-      html +=   '<title>' + config.title + '</title>';
-      html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">'
-      html += '</head>';
-      html += '<body>';
-      html += '  <h1>' + config.title + '</h1>';
-      html += '  <form method="GET" action="">';
-      html += '    <input type="text" name="title">';
-      html += '    <input type="submit" value="Change Gallery Title">';
-      html += '  </form>';
-      html += imageNamesToTags(imageTags).join('');
-      html += ' <form action="" method="POST" enctype="multipart/form-data">';
-      html += '   <input type="file" name="image">';
-      html += '   <input type="submit" value="Upload Image">';
-      html += ' </form>';
-      html += '</body>';
-  return html;
+  return template.render('gallery.html', {
+    title: config.title,
+    imageTags: imageNamesToTags(imageTags).join('')
+  });
 }
 
 /** @function serveGallery
